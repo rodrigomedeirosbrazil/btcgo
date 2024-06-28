0eda5aef9c00a2ffecc1a67643797033796c92a5
Rodrigo Medeiros <rodrigomdros@turno.com>
2024-06-28 17:01:34 -0300

    chore: add p2p.go

---
+++ src/p2p.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/libp2p/go-libp2p"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+)
+
+func main() {
+	node, err := libp2p.New()
+	// node, err := libp2p.New(
+	// 	libp2p.ListenAddrStrings("/ip4/192.0.2.0/tcp/0"),
+	// 	libp2p.Ping(false),
+	// )
+
+	if err != nil {
+		panic(err)
+	}
+
+	// configure our own ping protocol
+	pingService := &ping.PingService{Host: node}
+	node.SetStreamHandler(ping.ID, pingService.PingHandler)
+
+	// print the node's listening addresses
+	fmt.Println("Listen addresses:", node.Addrs())
+
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := peerstore.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	fmt.Println("libp2p node address:", addrs[0])
+
+	// shut the node down
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+
+	// wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+
+	// shut the node down
+	if err := node.Close(); err != nil {
+		panic(err)
+	}
+}
